ddfdd0e9c9bcd693a522ccd2bac4cf809df94a64
JoseLRIano <joselriano@yahoo.com>
2018-01-23 00:16:37 -0500

    Add Some basic CSS

--- fetch.js
+++ fetch.js
@@ -1,10 +1,13 @@
 var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
+
+
 fetch(url)
     .then(function(response){
     console.log(response);
     return response.json()
 })
 .then(function(data){
-    console.log(data.bpi.EUR.rate)
+     console.log(data.bpi.EUR.rate)
+
 });
